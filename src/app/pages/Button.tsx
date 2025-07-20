7f9ea2679607a7984282e1e94dd87b990319de77
jldec <jurgen@jldec.me>
2025-07-20 10:00:00 -0400

    init-rwsdk-minimal-w-button

---
+++ src/app/pages/Button.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { useState } from "react";
+
+export function Button() {
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>Click me {count}</button>;
+}
---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./Button";
+import { RequestInfo } from "rwsdk/worker";
+
+export function Home({ ctx }: RequestInfo) {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+    </div>
+  );
+}
